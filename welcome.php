ab9a169d2273e550f0d03ff0a6a4e575176044cf
SherlockiDTech <108091973+SherlockiDTech@users.noreply.github.com>
2023-11-21 12:13:46 -0600

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>title</title>
+</head>
+<body>
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+</body>
+</html>
